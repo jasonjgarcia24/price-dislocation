9469322cb85de8f303866b8142c77ba680274d39
JJGarcia24 <40121872+jasonjgarcia24@users.noreply.github.com>
2021-07-02 10:53:59 +0300

    Updated test_fileio() within \tests\test_qualifier.py.

--- loan-qualifier/qualifier/tests/test_qualifier.py
+++ loan-qualifier/qualifier/tests/test_qualifier.py
@@ -1,6 +1,7 @@
+import os
+
 # Import pathlib
 from pathlib import Path
-from qualifier.filters.loan_to_value import filter_loan_to_value
 
 #Import fileio
 from qualifier.utils import fileio
@@ -14,16 +15,21 @@ from qualifier.filters.debt_to_income import filter_debt_to_income
 from qualifier.filters.loan_to_value  import filter_loan_to_value
 from qualifier.filters.max_loan_size  import filter_max_loan_size
 
+test_path = os.path.dirname(os.path.realpath(__file__))
+prnt_path = os.path.dirname(test_path)
+
 # @TODO: Your code here!
 # Use Path from pathlib to output the test csv to ./data/output/qualifying_loans.csv
 def test_calculate_monthly_debt_ratio():
     assert calculators.calculate_monthly_debt_ratio(1500, 4000) == 0.375
+    assert calculators.calculate_monthly_debt_ratio(1500, 4000) != 0.370
 
 def test_calculate_loan_to_value_ratio():
     assert calculators.calculate_loan_to_value_ratio(210000, 250000) == 0.84
+    assert calculators.calculate_loan_to_value_ratio(210000, 250000) != 0.80
 
 def test_filters():
-    bank_data = fileio.load_csv(Path(r'../data/daily_rate_sheet.csv'))
+    bank_data = fileio.load_csv(Path(f"{prnt_path}\\data\\daily_rate_sheet.csv"))
     current_credit_score = 750
     debt                 = 1500
     income               = 4000
@@ -36,38 +42,50 @@ def test_filters():
 
     def test_filter_credit_score():
         # credit_score >= bank_data[4]
-        test_bank_data = [[0, 0, 0, 0, current_credit_score]]
-        assert filter_credit_score(current_credit_score, test_bank_data) == test_bank_data
+        test_bank_data = [[0, 0, 0, 0, current_credit_score]]       # Tests for ==      --TRUE condition
+        assert filter_credit_score(current_credit_score,   test_bank_data) == test_bank_data
 
-        test_bank_data = [[0, 0, 0, 0, current_credit_score+1]]
+        test_bank_data = [[0, 0, 0, 0, current_credit_score+1]]     # Tests for >      --TRUE condition
         assert filter_credit_score(current_credit_score+1, test_bank_data) == test_bank_data
+
+        test_bank_data = [[0, 0, 0, 0, current_credit_score+1]]     # Tests for <      --FALSE condition
+        assert filter_credit_score(current_credit_score, test_bank_data) != test_bank_data
 
 
     def test_debt_to_income():
         # monthly_debt_ratio <= bank_data[3]
-        test_bank_data = [[0, 0, 0, dti_ratio]]
-        assert filter_debt_to_income(dti_ratio, test_bank_data) == test_bank_data
+        test_bank_data = [[0, 0, 0, dti_ratio]]     # Tests for ==      --TRUE condition
+        assert filter_debt_to_income(dti_ratio,   test_bank_data) == test_bank_data
 
-        test_bank_data = [[0, 0, 0, dti_ratio-1]]
+        test_bank_data = [[0, 0, 0, dti_ratio-1]]   # Tests for <       --TRUE condition
         assert filter_debt_to_income(dti_ratio-1, test_bank_data) == test_bank_data
+
+        test_bank_data = [[0, 0, 0, dti_ratio-1]]   # Tests for >       --FALSE condition
+        assert filter_debt_to_income(dti_ratio, test_bank_data) != test_bank_data
 
 
     def test_loan_to_value():
         # loan_to_value_ratio <= bank_data[2]
-        test_bank_data = [[0, 0, ltv_ratio]]
-        assert filter_loan_to_value(ltv_ratio, test_bank_data) == test_bank_data
+        test_bank_data = [[0, 0, ltv_ratio]]        # Tests for ==      --TRUE condition
+        assert filter_loan_to_value(ltv_ratio,   test_bank_data) == test_bank_data
 
-        test_bank_data = [[0, 0, ltv_ratio-1]]
+        test_bank_data = [[0, 0, ltv_ratio-1]]      # Tests for <      --TRUE condition
         assert filter_loan_to_value(ltv_ratio-1, test_bank_data) == test_bank_data
+
+        test_bank_data = [[0, 0, ltv_ratio-1]]      # Tests for >      --FALSE condition
+        assert filter_loan_to_value(ltv_ratio, test_bank_data) != test_bank_data
 
     
     def test_max_loan_size():
         # loan_amount <= bank_data[1]
-        test_bank_data = [[0, loan]]
-        assert filter_max_loan_size(loan, test_bank_data) == test_bank_data
+        test_bank_data = [[0, loan]]        # Tests for ==      --TRUE condition
+        assert filter_max_loan_size(loan,   test_bank_data) == test_bank_data
 
-        test_bank_data = [[0, loan-1]]
+        test_bank_data = [[0, loan-1]]      # Tests for <      --TRUE condition
         assert filter_max_loan_size(loan-1, test_bank_data) == test_bank_data
+
+        test_bank_data = [[0, loan-1]]      # Tests for >      --FALSE condition
+        assert filter_max_loan_size(loan, test_bank_data) != test_bank_data
 
 
     test_filter_credit_score()
@@ -78,7 +96,17 @@ def test_filters():
 
     # @TODO: Test the new save_csv code!
     # YOUR CODE HERE!
-    def test_save_csv():    
-        csvpath = Path(r".\data\output\qualifying_loans.csv")
+def test_fileio():
+    def test_save_csv():
+        csvpath = Path(f"{test_path}\\qualifying_loans.csv")
+        bank_data = [
+            ['r1c1','r1c2','r1c3','r1c4','r1c5','r1c6'],
+            ['r2c1','r2c2','r2c3','r2c4','r2c5',],
+            ['r3c1','r3c2',None,'r3c4',None,],
+            [None]
+            ]
         assert fileio.save_csv(csvpath, bank_data) == True
+        os.remove(csvpath)
+
+    test_save_csv()
 
